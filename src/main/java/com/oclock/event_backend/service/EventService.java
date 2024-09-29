c46bb12f6f322ca35c3023f33882f5382d4180eb
NINIAA <niniaa.fatimaezzahraa@gmail.com>
2024-09-29 22:28:43 +0200

    feat: EC0022 implement getEventById endpoint to fetch a specific event details

--- src/main/java/com/oclock/event_backend/service/EventService.java
+++ src/main/java/com/oclock/event_backend/service/EventService.java
@@ -4,4 +4,5 @@ import com.oclock.event_backend.dto.EventDto;
 
 public interface EventService {
     EventDto createEvent(EventDto eventDto, String managerUsername);
+    EventDto getEventById(Long eventId);
 }
