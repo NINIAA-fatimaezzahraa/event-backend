cffbc3dc0d59911a4906c2742c5cd790d62d3b83
NINIAA <niniaa.fatimaezzahraa@gmail.com>
2024-09-30 12:26:43 +0200

    feat: EC0024 implement getEventsByCategory endpoint to fetch a selected category events

--- src/main/java/com/oclock/event_backend/repository/EventRepository.java
+++ src/main/java/com/oclock/event_backend/repository/EventRepository.java
@@ -1,9 +1,14 @@
 package com.oclock.event_backend.repository;
 
 import com.oclock.event_backend.domain.Event;
+import com.oclock.event_backend.domain.EventCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    Set<Event> findByCategory(EventCategory category);
 }
